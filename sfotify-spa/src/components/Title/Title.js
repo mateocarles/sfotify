8137ed075db42770f9dc24912b785be937b318fe
Induction <inductionv8@gmail.com>
2017-04-17 19:32:58 -0300

    Added Title.css to component

--- sfotify-spa/src/components/Title/Title.js
+++ sfotify-spa/src/components/Title/Title.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import classnames from 'classnames'
 
 class Title extends Component {
   render() {
     const title = this.props.texts;
+    const styleName = this.props.styleName;
     return (
       <div>
-        <h1> {title} </h1>
+        <h1 className={classnames('default', styleName)}> {title} </h1>
       </div>
     );
   }
