4f0069a06008142bf8019b49bf6114cc5726e51a
Ale Paciotti <alejandro.paciotti@gmail.com>
2017-04-18 15:20:48 -0300

    Agregado Home al contenedor App

---
+++ sfotify-spa/src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Home extends Component {
+  render() {
+    return (
+      
+      <h1> HOME </h1>
+
+    );
+  }
+}
+
+export default Home;
