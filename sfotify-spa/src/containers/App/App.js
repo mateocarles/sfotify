b7b731aa61b37272342f12ca2a0c650590653da8
Ale Paciotti <alejandro.paciotti@gmail.com>
2017-04-19 10:41:39 -0300

    Agregado actions reducers y componente Favorite Song Container

--- sfotify-spa/src/containers/App/App.js
+++ sfotify-spa/src/containers/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Home from '../Home/Home'
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
--- sfotify-spa/src/containers/Home/Home.js
+++ sfotify-spa/src/containers/Home/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import FavoriteSongsContainer from '../../components/FavoriteSongsContainer/FavoriteSongsContainer';
 
 class Home extends Component {
   render() {
     return (
       
-      <h1> HOME </h1>
+      <FavoriteSongsContainer />
 
     );
   }
