88aa7a4fd6fca15f580e2f28eb4acf30512feb50
Mateo Carles <mateo2291@gmail.com>
2017-04-27 20:31:21 -0300

    up to 2nd feature

--- sfotify-spa/src/components/FavoriteSongsContainer/FavoriteSongsContainer.js
+++ sfotify-spa/src/components/FavoriteSongsContainer/FavoriteSongsContainer.js
@@ -1,48 +1,14 @@
+
 import React, { Component } from 'react';
 import FavoriteSongBox from  './FavoriteSongBox';
-import Request from 'superagent';
-import _ from 'lodash';
 
 class FavoriteSongsContainer extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      songs: [
-      ]
-    }
-
-  }
-
-  componentWillMount() {
-    this.search();
-  }
-
-  uSearch(val) {
-    this.search(val);
-  }
-
-  search(val = "j") {
-    var url = `https://api.spotify.com/v1/search?type=artist&q=${val}`;
-    Request.get(url).then((response) => {
-      this.setState({
-        songs: response,        
-      });
-    });
-  }
-
-  
 
   render() {
-    const songs = this.state.songs.map( (item, idx)  => {
-        return <li key={idx}> {item} </li>
-    })
     return (
       <div>
-        <h2> Canciones </h2>
-        <ul>
-          {songs}
-        </ul>
+        <h2> Songs </h2>
+        <h3>{this.props.artist}</h3>
       </div>
     );
   }
--- sfotify-spa/src/components/InputSearch/InputSearch.js
+++ sfotify-spa/src/components/InputSearch/InputSearch.js
@@ -1,20 +1,103 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import Request from 'superagent';
+import './InputSearch.css';
 import FavSong from '../FavoriteSongsContainer/FavoriteSongsContainer';
 
 class InputSearch extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      songs: [],
+      data: [],
+      id: [],
+      albums: []
+    }
+  }
+
+  handleChange(event) {
+    const query = event.target.value
+    this.setState({query: query})
+    this.submitSearch(query)
+  }
+
+  submitSearch(keyword) {
+    const url = `https://api.spotify.com/v1/search?type=artist&q=${keyword}`;
+    Request.get(url).then((response) => {
+      //Pass the parameters to another component to render the images?
+      console.log("El del submit: " + response.body.artists.items[0].name);
+      console.log(response.body.artists.items[0]);
+
+      var arr = [];
+      var arrId =[];
+
+      for (var i in response.body.artists.items) {
+        arr.push(response.body.artists.items[i].name);
+        arrId.push(response.body.artists.items[i].id);
+      }
+
+      console.log("El array: " + arr);
+
+      this.setState({songs: arr});
+      this.setState({data: response.body.artists.items})
+      this.setState({id:response.body.artists.items.id})
+
+
+      console.log("DATA: " + this.state.data);
+      console.log("ID: " + arrId);
+    });
+  }
+
+handleClick(key){
+  
+  for(var i in this.state.data) {
+    if(this.state.data[i].name==key)
+    {
+      const id = this.state.data[i].id;
+      const url = ` https://api.spotify.com/v1/artists/${id}/albums`;
+      Request.get(url).then((response) => {
+
+
+        var arrAlb = [];
+
+
+        for (var i in response.body.items) {
+        arrAlb.push(response.body.items[i].name);
+        }
+      
+        this.setState({albums: arrAlb})
+
+
+      });
+    }
+  }  
+}
+
+
   render() {
-    return ( 
+    var self = this;
+    return (
       <div>
-        <input type = "text" ref="val" onChange={ (e) => {this.updateSearch(); } }  id = "inputSearch" placeholder = "Search the name of your favorite artist">
-        </input>
+        <input type="text" ref="val" onChange={this.handleChange.bind(this)} id="inputSearch" placeholder="Search the name of your favorite artist"/>
+        <ul>
+          {this.state.songs.map(function(listValue,i){
+            return <li key={i}><a  href="#" onClick={() =>{
+              {self.handleClick(listValue)}
+            }}>{listValue}</a></li>;
+          })}
+        </ul>
+        <h2>Albums</h2>
+         <ul>
+          {this.state.albums.map(function(listAlbum,i){
+            return <li key={i}>{listAlbum}</li>;
+          })}
+        </ul>
+
+        
       </div>
     )
   }
-  updateSearch() {    
-    FavSong.uSearch(this.refs.val.value);
-  }
 }
-
 
 export default InputSearch;
