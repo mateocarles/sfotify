7b53f9dd9cf2b43a5ca591c713524bb46909f2dc
Mateo Carles <mateo2291@gmail.com>
2017-04-28 12:17:11 -0300

    Mateo y Rodrigo -Add cover-Final Commit

--- sfotify-spa/src/components/InputSearch/InputSearch.js
+++ sfotify-spa/src/components/InputSearch/InputSearch.js
@@ -13,19 +13,31 @@ class InputSearch extends React.Component {
       data: [],
       id: [],
       albums: [],
-      songs:[],
+      songs: [],
       idAlb: [],
-      songsPrv: []
+      songsPrv: [],
+      cover: ''
     }
   }
 
   handleChange(event) {
     const query = event.target.value
+    // Don't duplicate searches or send redundant network requests
+    if (this.state.query === query) {
+      return
+    }
     this.setState({query: query})
     this.submitSearch(query)
   }
 
   submitSearch(keyword) {
+    // If the keyword isn't present, set the data object to an empty object and
+    // set a flag that the input has been cleared so we don't render any
+    // pending AJAX requests after the user clears the input box
+    if (!keyword.length > 0) {
+      this.setState({query: '', artists: []})
+      return
+    }
     const url = `https://api.spotify.com/v1/search?type=artist&q=${keyword}`;
     Request.get(url).then((response) => {
       //Pass the parameters to another component to render the images?
@@ -33,7 +45,7 @@ class InputSearch extends React.Component {
       console.log(response.body.artists.items[0]);
 
       var arr = [];
-      var arrId =[];
+      var arrId = [];
 
       for (var i in response.body.artists.items) {
         arr.push(response.body.artists.items[i].name);
@@ -44,86 +56,73 @@ class InputSearch extends React.Component {
 
       this.setState({artists: arr});
       this.setState({data: response.body.artists.items})
-      this.setState({id:response.body.artists.items.id})
-
+      this.setState({id: response.body.artists.items.id})
 
       console.log("DATA: " + this.state.data);
       console.log("ID: " + arrId);
     });
   }
 
-  handleClickArtist(key){
-    
-    for(var i in this.state.data) {
-      if(this.state.data[i].name==key)
-      {
+  handleClickArtist(key) {
+
+    for (var i in this.state.data) {
+      if (this.state.data[i].name == key) {
         const id = this.state.data[i].id;
         const url = ` https://api.spotify.com/v1/artists/${id}/albums`;
         Request.get(url).then((response) => {
 
-
           var albArray = [];
           var albIdArray = [];
-
 
           for (var i in response.body.items) {
             albArray.push(response.body.items[i].name);
             albIdArray.push(response.body.items[i].id);
           }
 
-        
-        
           this.setState({albums: albArray})
           this.setState({idAlb: albIdArray})
 
           console.log(albIdArray);
-         
+
         });
       }
-    }  
+    }
   }
-  handleClickAlbum(key){
-    
-    for(var i in this.state.albums) {
-      if(this.state.albums[i]===key) {             
-        
+  handleClickAlbum(key) {
+
+    for (var i in this.state.albums) {
+      if (this.state.albums[i] === key) {
+
         const id = this.state.idAlb[i];
         const url = ` https://api.spotify.com/v1/albums/${id}`;
         Request.get(url).then((response) => {
 
-
           var arrSongs = [];
           var arrPrv = [];
+          var cover = '';
 
           for (var i in response.body.tracks.items) {
             arrSongs.push(response.body.tracks.items[i].name);
             arrPrv.push(response.body.tracks.items[i].preview_url);
+            cover = response.body.images[0].url;
           }
-          
-          
+
           this.setState({songs: arrSongs})
           this.setState({songsPrv: arrPrv})
-
-          
+          this.setState({cover: cover})
         });
       }
-    } 
+    }
   }
 
   handleClickSong(key) {
-    for(var i in this.state.songs) {
-      if(this.state.songs[i]===key) {
-
+    for (var i in this.state.songs) {
+      if (this.state.songs[i] === key) {
         const id = this.state.songsPrv[i];
         const url = `${id}`
-        Request.get(url).then((response) => {
-
-
-        })
-
+        Request.get(url).then((response) => {})
       }
     }
-    
   }
 
   render() {
@@ -132,32 +131,41 @@ class InputSearch extends React.Component {
       <div>
         <input type="text" ref="val" onChange={this.handleChange.bind(this)} id="inputSearch" placeholder="Search the name of your favorite artist"/>
         <ul>
-          {this.state.artists.map(function(listValue,i){
-            return <li key={i}><a  href="#" onClick={() =>{
-              {self.handleClickArtist(listValue)}
-            }}>{listValue}</a></li>;
+          {this.state.artists.map(function(listValue, i) {
+            return <li key={i}>
+              <a href="#" onClick={() => {
+                {
+                  self.handleClickArtist(listValue)
+                }
+              }}>{listValue}</a>
+            </li>;
           })}
         </ul>
-
-
 
         <h2>Albums</h2>
-         <ul>
-          {this.state.albums.map(function(listAlbum,i){
-            return <li key={i}><a  href="#" onClick={() =>{
-              {self.handleClickAlbum(listAlbum)}
-            }}>{listAlbum}</a></li>;
-          })}
-        </ul>
-       
-        <h2>Songs input</h2>
-        
         <ul>
-           {this.state.songs.map(function(listSong,i){
-            return <li key={i}><a  href={self.state.songsPrv[i]}>{listSong}</a></li>;
+          {this.state.albums.map(function(listAlbum, i) {
+            return <li key={i}>
+              <a href="#" onClick={() => {
+                {
+                  self.handleClickAlbum(listAlbum)
+                }
+              }}>{listAlbum}</a>
+            </li>;
+          })}
+          <img src={this.state.cover} alt=""/>
+        </ul>
+
+        <h2>Songs input</h2>
+
+        <ul>
+          {this.state.songs.map(function(listSong, i) {
+            return <li key={i}>
+              <a href={self.state.songsPrv[i]}>{listSong}</a>
+            </li>;
           })}
         </ul>
-        
+
       </div>
     )
   }
